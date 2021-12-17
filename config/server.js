5ab6ed2f5863e40244498d1689b37c97efe818d7
mproznydev <mateuszprozny@gmail.com>
2021-12-17 15:01:26 +0100

    second commit

--- config/server.js
+++ config/server.js
@@ -1,8 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url:'https://ufiolkow-oms.herokuapp.com/',
   proxy: true,
   admin: {
+    url:'/',
+    serverAdminPanel:false,
     auth: {
       secret: env('ADMIN_JWT_SECRET', '23ae3722870fd6323ac3fbe4ffdd1e50'),
     },
