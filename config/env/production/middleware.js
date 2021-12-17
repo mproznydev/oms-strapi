e794e00631fc324eaa8eeb6d5f0c4423f098da0f
mproznydev <mateuszprozny@gmail.com>
2021-12-17 15:23:12 +0100

    middleware update with proper cors

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -5,7 +5,7 @@ module.exports = {
         origin: [
           "http://localhost:3000",
            "http://localhost:1337",
-           "https://naughty-yalow-aa259a.netlify.app/"
+           "https://naughty-yalow-aa259a.netlify.app"
         ],
       },
     },
