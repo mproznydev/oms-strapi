affe3d85f6ac6dda6bbce2585ac128c74377bd71
mproznydev <mateuszprozny@gmail.com>
2021-12-18 10:04:53 +0100

    without proxy

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,5 @@
 module.exports = ({ env }) => ({
     url: 'https://ufiolkow-oms.herokuapp.com/',
-    proxy: true,
     admin: {
         auth: {
           secret: env('ADMIN_JWT_SECRET', '23ae3722870fd6323ac3fbe4ffdd1e50'),
