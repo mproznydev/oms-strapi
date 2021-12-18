2eb583a41cc6b920c052c63019ec6a05ef7c2d32
mproznydev <mateuszprozny@gmail.com>
2021-12-18 09:58:28 +0100

    change url to proper link

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-    url: 'https://naughty-yalow-aa259a.netlify.app',
+    url: 'https://ufiolkow-oms.herokuapp.com/',
     proxy: true,
     admin: {
         auth: {
-          secret: '28e4cc49-f7c4-41b1-9f5d-2acbe7c38651',
+          secret: env('ADMIN_JWT_SECRET', '23ae3722870fd6323ac3fbe4ffdd1e50'),
         },
       },
   });
