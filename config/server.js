a0f9a1178c350f6b0df19a08355280e623691bec
mproznydev <mateuszprozny@gmail.com>
2021-12-18 09:17:39 +0100

    domain delete

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url:'https://ufiolkow-oms.herokuapp.com/',
   proxy: true,
   admin: {
     auth: {
