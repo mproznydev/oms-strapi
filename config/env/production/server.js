dd128db375c1b18bd105a3cd5a14c53d1d8bc766
mproznydev <mateuszprozny@gmail.com>
2021-12-18 09:27:06 +0100

    secret auth added

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
     url: env('MY_HEROKU_URL'),
+    admin: {
+        auth: {
+          secret: env('ADMIN_JWT_SECRET', '23ae3722870fd6323ac3fbe4ffdd1e50'),
+        },
+      },
   });
    
