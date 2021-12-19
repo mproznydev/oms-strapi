3f85da8b35351d8d3a9a11779255d5b82d0ab4c4
mproznydev <mateuszprozny@gmail.com>
2021-12-19 18:12:52 +0100

    env improve

--- config/middleware.js
+++ config/middleware.js
@@ -1,12 +1,12 @@
-module.exports = {
-    //...
-    settings: {
-      cors: {
-        origin: [
-          "http://localhost:3000",
-           "http://localhost:1337",
-           "https://naughty-yalow-aa259a.netlify.app/"
-        ],
-      },
-    },
-  };
+// module.exports = {
+//     //...
+//     settings: {
+//       cors: {
+//         origin: [
+//           "http://localhost:3000",
+//            "http://localhost:1337",
+//            "https://naughty-yalow-aa259a.netlify.app/"
+//         ],
+//       },
+//     },
+//   };
