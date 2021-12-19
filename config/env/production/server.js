fceb5f341aec570f412025969ed9b61215869aa8
mproznydev <mateuszprozny@gmail.com>
2021-12-19 14:17:37 +0100

    add url from heroku

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,4 @@
 module.exports = ({ env }) => ({
-    url: 'https://ufiolkow-oms.herokuapp.com',
-    admin: {
-        auth: {
-          secret: env('ADMIN_JWT_SECRET', '23ae3722870fd6323ac3fbe4ffdd1e50'),
-        },
-      },
+    url: env('MY_HEROKU_URL'),
   });
    
