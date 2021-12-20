b6da511c7a5be1ee2e37ff4bb257f139dad6c72d
mproznydev <mateuszprozny@gmail.com>
2021-12-20 15:34:03 +0100

    cors enable

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -2,7 +2,8 @@ module.exports = {
     //...
     settings: {
       cors: {
-        origin: [
+          enabled:true,
+            origin: [
           "http://localhost:3000",
            "http://localhost:1337",
            "https://naughty-yalow-aa259a.netlify.app",
--- config/middleware.js
+++ config/middleware.js
@@ -1,12 +1,15 @@
-// module.exports = {
-//     //...
-//     settings: {
-//       cors: {
-//         origin: [
-//           "http://localhost:3000",
-//            "http://localhost:1337",
-//            "https://naughty-yalow-aa259a.netlify.app/"
-//         ],
-//       },
-//     },
-//   };
+module.exports = {
+    //...
+    settings: {
+      cors: {
+          enabled:true,
+            origin: [
+          "http://localhost:3000",
+           "http://localhost:1337",
+           "https://naughty-yalow-aa259a.netlify.app",
+           "https://ufiolkow-oms.herokuapp.com"
+
+        ],
+      },
+    },
+  };
